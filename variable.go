102bc9b5dd2ac69832c41cf4cf43efe8a26cfabd
rafi ahamed <cityinstitute1992@gmail.com>
2021-02-23 23:52:47 +0600

    variable program

---
+++ variable.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main( ){
+
+//var name string="rafi"
+//var name2 ,city string="rafi","dhaka"
+
+//fmt.Println(name,city,name2)
+
+//} 
+
+/*
+var name string="rafi"
+var name2 ,city string="rafi","dhaka"
+
+fmt.Println(name,city,name2)
+*/
+//char
+var age int = 10
+var result float32=50.95
+
+fmt.Println(age,result)
+
+}
